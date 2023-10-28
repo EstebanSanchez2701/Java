bf69e3e4d6438a1815d005c7896b091e57cf3443
EstebanSanchez2701 <eim.sanchezbaez@gmail.com>
2023-10-27 21:18:31 -0500

    Ejercicio 34

--- conexionbdd/src/main/java/com/krakedev/persistencia/test/TestConsultarUsuarioApellido.java
+++ conexionbdd/src/main/java/com/krakedev/persistencia/test/TestConsultarUsuarioApellido.java
@@ -1,19 +1,20 @@
 package com.krakedev.persistencia.test;
 
+import java.util.ArrayList;
+
 import com.krakedev.persistencia.entidades.Usuario;
 import com.krakedev.persistencia.servicios.AdminUsuarios;
 
-public class TestConsultarUsuarios {
+public class TestConsultarUsuarioApellido {
 
 	public static void main(String[] args) {
 		try {
-			Usuario usuarioEncontrado = AdminUsuarios.buscarCedulaUsuario("0192837465");
-			System.out.println(usuarioEncontrado);
+			ArrayList<Usuario> usuarios = AdminUsuarios.buscarPorApellido("A");
+			System.out.println(usuarios);
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println(e.getMessage());
 		}
-
 	}
 
 }
